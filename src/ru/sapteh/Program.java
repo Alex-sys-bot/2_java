5a37abed3e1a3c9177682973619d06b9833d6dd0
AlexLifeless <lin26618@gmail.com>
2020-10-06 19:34:46 +0300

    LenthArray

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -1,44 +1,20 @@
 package ru.sapteh;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Program {
-    public static void main(String[] args) {
-        String[] arr ={"hello","World"};
-        System.out.println(strSort(arr));
-        String str = "radar";
-        System.out.println(isPalindrome(str));
-    }
-
-    public static String strSort(String[] arr) {
-        int maxLengthString = 0;
-        String s="";
+    public static void main(String[] args)throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите кол-во элементов массива");
+        int index = Integer.parseInt(buffer.readLine());
+        int[] arr = new int[index];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length() > maxLengthString) {
-                maxLengthString = arr[i].length();
-            }
+            arr[i] += (Math.random() * 10) - 5;
+            System.out.printf("%d ",arr[i]);
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length() == maxLengthString) {
-                s = arr[i];
-            }
-        }
-        return s;
-    }
-
-    public static String reverseString(String s) {
-        String r = "";
-        for (int i = s.length() - 1; i >= 0; --i){
-            r += s.charAt(i);
-        }
-        return r;
-    }
-
-    public static Boolean isPalindrome(String s) {
-        if (s.equals(reverseString(s))) {
-            System.out.println("Полиндром");
-        } else {
-            System.out.println("Не полиндром");
-        }
-        return s.equals(reverseString(s));
     }
 }
+
