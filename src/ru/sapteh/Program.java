6fba4b4aeaa8dc68cdf38ed15ad4c8f124827797
AlexLifeless <lin26618@gmail.com>
2020-10-07 11:45:07 +0300

    sum digitals

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -1,6 +1,5 @@
 package ru.sapteh;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,12 +7,14 @@ import java.io.InputStreamReader;
 public class Program {
     public static void main(String[] args)throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите кол-во элементов массива");
-        int index = Integer.parseInt(buffer.readLine());
-        int[] arr = new int[index];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] += (Math.random() * 10) - 5;
-            System.out.printf("%d ",arr[i]);
+        System.out.println("Введите числа, которые хотите сложить: ");
+
+        int sum = 0;
+        while (true){
+            int input = Integer.parseInt(buffer.readLine());
+            sum += input;
+            if (input == -1) break;
+            System.out.println(sum);
         }
     }
 }
